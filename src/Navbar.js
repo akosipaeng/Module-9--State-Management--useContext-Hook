3ef5b0cdf7516cf4d78bc838a285913e91a06412
akosipaeng <mr.vasquezr@gmail.com>
2023-09-19 19:19:12 +0800

    Module 9: State Management, useContext Hook

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+export const Navbar = () => {
+  return (
+    <div>
+      <Link to="/"> HOME </Link>
+      <Link to="/profile"> PROFILE </Link>
+      <Link to="/contact"> CONTACT </Link>
+    </div>
+  );
+};
